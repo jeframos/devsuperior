26e571751d05dcfb33f38d79b76bea63e11472b4
Jefferson Ramos <jefferson.ramos10@gmail.com>
2025-02-14 20:34:48 -0300

    Entities Activity Category

---
+++ java-spring-professional/dsevent/src/main/java/com/devsuperior/dsevent/entities/Category.java
@@ -0,0 +1,69 @@
+package com.devsuperior.dsevent.entities;
+
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_category")
+public class Category {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(columnDefinition = "TEXT")
+	private String description;
+	
+	@OneToMany()
+	private Set<Activity> activities = new HashSet<>();
+	
+	public Category() {}
+
+	public Category(Integer id, String description) {
+		this.id = id;
+		this.description = description;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(description, id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		return Objects.equals(description, other.description) && Objects.equals(id, other.id);
+	}
+
+}
