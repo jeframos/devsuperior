92bc5668fec554cdb69d0722443a871cfa5c3dc4
Jefferson Ramos <jefferson.ramos10@gmail.com>
2025-02-15 22:19:42 -0300

    Get product by id

---
+++ java-spring-professional/dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -0,0 +1,24 @@
+package com.devsuperior.dscommerce.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dscommerce.dto.ProductDto;
+import com.devsuperior.dscommerce.service.ProductService;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductController {
+	
+	@Autowired
+	private ProductService service;
+	
+	@GetMapping(value = "/{id}")
+	public ProductDto findById(@PathVariable Long id) {
+		return service.findById(id);
+	}
+
+}
---
+++ java-spring-professional/dscommerce/src/main/java/com/devsuperior/dscommerce/service/ProductService.java
@@ -0,0 +1,25 @@
+package com.devsuperior.dscommerce.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dscommerce.dto.ProductDto;
+import com.devsuperior.dscommerce.entities.Product;
+import com.devsuperior.dscommerce.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	@Transactional(readOnly = true)
+	public ProductDto findById(Long id) {
+		Product product = repository.findById(id).get();
+		return new ProductDto(product);
+	}
+
+}
