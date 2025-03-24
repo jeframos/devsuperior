f0ce5afb7e7403f72acb4a049da0839b79e8c4c6
Jefferson Ramos <jefferson.ramos10@gmail.com>
2025-03-24 13:27:38 -0300

    Update seed

--- java-spring-professional/dscrud/src/main/resources/import.sql
+++ java-spring-professional/dscrud/src/main/resources/import.sql
@@ -1,3 +1,11 @@
-INSERT INTO tb_client (name, cpf, income, children, birth_date) VALUES ('Maria Helena', '97903465813', 15000.00, 1, '1962-06-30');
-INSERT INTO tb_client (name, cpf, income, children, birth_date) VALUES ('Marilene Rodrigues', '89449224625', 18000.00, 2, '1968-09-15');
-INSERT INTO tb_client (name, cpf, income, children, birth_date) VALUES ('Pedro Henrique Rodrigues', '14137220085', 25000.00, 0, '2015-12-22');
+INSERT INTO tb_client (name, cpf, income, children, birth_date) VALUES ('Maria Helena', '97903465813', 35000.00, 1, '1962-06-30');
+INSERT INTO tb_client (name, cpf, income, children, birth_date) VALUES ('Marilene Rodrigues', '89449224625', 35000.00, 2, '1968-09-15');
+INSERT INTO tb_client (name, cpf, income, children, birth_date) VALUES ('Pedro Henrique Rodrigues', '14137220085', 25000.00, 0, '1999-12-22');
+INSERT INTO tb_client (name, cpf, income, children, birth_date) VALUES ('Beatriz Ramos', '74566164349', 40000.00, 0, '1988-08-13');
+INSERT INTO tb_client (name, cpf, income, children, birth_date) VALUES ('Manuela Rodrigues', '84724256060', 25000.00, 0, '2000-04-23');
+INSERT INTO tb_client (name, cpf, income, children, birth_date) VALUES ('Valdecir Ramos', '04891842350', 20000.00, 0, '1962-12-13');
+INSERT INTO tb_client (name, cpf, income, children, birth_date) VALUES ('Gloria Nishida', '02402988029', 25000.00, 0, '1957-09-22');
+INSERT INTO tb_client (name, cpf, income, children, birth_date) VALUES ('Everton Santos', '23557965724', 25000.00, 0, '1987-04-23');
+INSERT INTO tb_client (name, cpf, income, children, birth_date) VALUES ('Rogerio Alberto', '14012473697', 25000.00, 0, '1973-06-04');
+INSERT INTO tb_client (name, cpf, income, children, birth_date) VALUES ('Luiz Fernando', '03290838218', 25000.00, 0, '1989-07-29');
+
