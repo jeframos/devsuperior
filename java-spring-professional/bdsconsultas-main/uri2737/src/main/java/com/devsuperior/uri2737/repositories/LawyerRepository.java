9836721053b986de13f36ec15cb60dc4256cdcbf
Jefferson Ramos <jefferson.ramos10@gmail.com>
2025-04-15 00:04:13 -0300

    add uti2737

--- java-spring-professional/bdsconsultas-main/uri2737/src/main/java/com/devsuperior/uri2737/repositories/LawyerRepository.java
+++ java-spring-professional/bdsconsultas-main/uri2737/src/main/java/com/devsuperior/uri2737/repositories/LawyerRepository.java
@@ -1,9 +1,36 @@
 package com.devsuperior.uri2737.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.devsuperior.uri2737.dto.LawyerMinDTO;
 import com.devsuperior.uri2737.entities.Lawyer;
+import com.devsuperior.uri2737.projections.LawyerMinProjection;
 
 public interface LawyerRepository extends JpaRepository<Lawyer, Long> {
+	
+	@Query(nativeQuery = true, value = "(SELECT name, customers_number AS customersNumber "
+			+ "  FROM lawyers "
+			+ " WHERE customers_number = ("
+			+ "	SELECT MAX(customers_number) "
+			+ "	  from lawyers "
+			+ " )) "
+			+ " "
+			+ "UNION ALL "
+			+ " "
+			+ "(SELECT name, customers_number "
+			+ "  FROM lawyers "
+			+ " WHERE customers_number = ("
+			+ "	SELECT MIN(customers_number) "
+			+ "	  from lawyers "
+			+ " )) "
+			+ ""
+			+ "UNION ALL "
+			+ " "
+			+ "(SELECT 'Average', ROUND(AVG(customers_number), 0) "
+			+ "   FROM lawyers)")
+	List<LawyerMinProjection> search1();
 
 }
