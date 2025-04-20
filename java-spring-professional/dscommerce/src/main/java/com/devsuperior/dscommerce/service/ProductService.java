1c05b6bc76eecab11a80eddbf72e488b8322174d
Jefferson Ramos <jefferson.ramos10@gmail.com>
2025-04-20 00:56:42 -0300

    Update method GET product

--- java-spring-professional/dscommerce/src/main/java/com/devsuperior/dscommerce/service/ProductService.java
+++ java-spring-professional/dscommerce/src/main/java/com/devsuperior/dscommerce/service/ProductService.java
@@ -31,8 +31,8 @@ public class ProductService {
 	}
 	
 	@Transactional(readOnly = true)
-	public Page<ProductDto> findAll(Pageable pageable) {
-		Page<Product> result = repository.findAll(pageable);
+	public Page<ProductDto> findAll(String name, Pageable pageable) {
+		Page<Product> result = repository.searchByName(name, pageable);
 		return result.map(product -> new ProductDto(product));
 	}
 
