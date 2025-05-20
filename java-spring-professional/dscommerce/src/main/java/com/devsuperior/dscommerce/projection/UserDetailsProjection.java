2ab3cf57d46402096b4936f9f1561d2c429a2ee9
Jefferson Ramos <jefferson.ramos10@gmail.com>
2025-05-19 22:48:04 -0300

    UserDetailsService

---
+++ java-spring-professional/dscommerce/src/main/java/com/devsuperior/dscommerce/projection/UserDetailsProjection.java
@@ -0,0 +1,9 @@
+package com.devsuperior.dscommerce.projection;
+
+public interface UserDetailsProjection {
+	
+	String getUserName();
+	String getPassword();
+	Long getRoleId();
+	String getAuthority();
+}
