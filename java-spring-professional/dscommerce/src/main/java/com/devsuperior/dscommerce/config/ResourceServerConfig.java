1f6f1cd3f925febb0eab32d6b20a815b3c47b0b9
Jefferson Ramos <jefferson.ramos10@gmail.com>
2025-05-22 00:24:46 -0300

    Fix: filterRegistrationBeancorsFilter method

--- java-spring-professional/dscommerce/src/main/java/com/devsuperior/dscommerce/config/ResourceServerConfig.java
+++ java-spring-professional/dscommerce/src/main/java/com/devsuperior/dscommerce/config/ResourceServerConfig.java
@@ -79,7 +79,7 @@ public class ResourceServerConfig {
 	}
 
 	@Bean
-	FilterRegistrationBean<CorsFilter> corsFilter() {
+	FilterRegistrationBean<CorsFilter> filterRegistrationBeancorsFilter() {
 		FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(
 				new CorsFilter(corsConfigurationSource()));
 		bean.setOrder(Ordered.HIGHEST_PRECEDENCE);
