0e1fcd597bbeaac5dc8eb1433e804cc3ad9d5a87
Jefferson Ramos <jefferson.ramos10@gmail.com>
2025-11-19 12:40:58 -0300

    Category Resource

---
+++ java-spring-expert/dscatalog/backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -0,0 +1,47 @@
+package com.devsuperior.dscatalog.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Category implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String Name;
+
+    public Category() {
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        Name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
