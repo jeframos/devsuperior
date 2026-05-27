a9e109bd88d273ac1d68ceb1eefcdfd85873a28d
Jefferson Ramos <jefferson.ramos10@gmail.com>
2026-05-27 15:58:15 -0300

    feat: Salvando uma nova senha caso o token seja válido

--- java-spring-expert/dscatalog/backend/src/main/java/com/devsuperior/dscatalog/services/AuthService.java
+++ java-spring-expert/dscatalog/backend/src/main/java/com/devsuperior/dscatalog/services/AuthService.java
@@ -1,16 +1,20 @@
 package com.devsuperior.dscatalog.services;
 
 import com.devsuperior.dscatalog.dto.EmailDTO;
+import com.devsuperior.dscatalog.dto.NewPasswordDTO;
 import com.devsuperior.dscatalog.entities.PasswordRecover;
 import com.devsuperior.dscatalog.entities.User;
 import com.devsuperior.dscatalog.repositories.PasswordRecoverRepository;
 import com.devsuperior.dscatalog.repositories.UserRepository;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -21,6 +25,9 @@ public class AuthService {
 
     @Value("${email.password-recover.uri}")
     private String recoverUri;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Autowired
     private UserRepository userRepository;
@@ -53,5 +60,19 @@ public class AuthService {
                 + recoverUri + token + ". Validade de " + tokenMinutes + " minutos";
 
         emailService.sendEmail(body.getEmail(), "Recuperação de senha", msgBody);
+    }
+
+    public void saveNewPassword(@Valid NewPasswordDTO body) {
+
+        //Verifica se o token informado na requisição é valido.
+        List<PasswordRecover> result = passwordRecoverRepository.searchValidTokens(body.getToken(), Instant.now());
+        if (result.size() == 0) {
+            throw new ResourceNotFoundException("Token inválido");
+        }
+
+        //Caso o token seja valido, atualizar a senha do usuário e salvar no banco de dados.
+        User user = userRepository.findByEmail(result.get(0).getEmail());
+        user.setPassword(passwordEncoder.encode(body.getPassword()));
+        userRepository.save(user);
     }
 }
